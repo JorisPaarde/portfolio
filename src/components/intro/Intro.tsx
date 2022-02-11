9b81b18cc4cf25462ac7bd92f69f9e5617a987b1
JorisPaarde <jorispaarde@gmail.com>
2022-02-11 11:44:09 +0100

    Added intro page

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,12 +1,45 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import styles from './Intro.module.scss';
+import { init } from 'ityped'
 
-interface IntroProps {}
+interface IntroProps { }
 
-const Intro: FC<IntroProps> = () => (
-  <div className={styles.Intro} id="intro">
-    Intro Component
-  </div>
-);
+const Intro: FC<IntroProps> = ({ }) => {
+
+  const textRef = useRef();
+
+  useEffect(() => init(textRef.current, {
+    showCursor: true,
+    backDelay: 1500,
+    backSpeed: 60,
+    cursorChar: ".",
+    strings: ['Developer', 'Designer', 'Content Creator'],
+  })
+    , []);
+
+
+  return (
+    <div className={styles.Intro} id="intro">
+      <div className={styles.left}>
+        <div className={styles.imgContainer}>
+          <img src="assets\man.png" alt="" />
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2>Hoi ik ben</h2>
+          <h1>Joris Paardekooper</h1>
+          <h3>Freelance <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="\assets\down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  )
+};
+
+
+
 
 export default Intro;
